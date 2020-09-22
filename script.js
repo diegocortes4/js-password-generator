c5e8b26180f4467a9576016bd4c89a130de14e89
Diego Cortes <diegocortes4@gmail.com>
2020-09-22 15:44:44 -0400

    added Arrey

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperLetters = ["A","B","C"]
+var lowerCase = ["a","b","c"]
+var numbers = ["1","2","3"]
+var symbols = ["!","?","@"]
+
+
+
+
  
 }
 // Add event listener to generate button

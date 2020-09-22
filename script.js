72cb8ca967f64c057cb82e647f11b00df3afa8db
Diego Cortes <diegocortes4@gmail.com>
2020-09-22 15:21:26 -0400

    Added original files

---
+++ script.js
@@ -0,0 +1,11 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+ 
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+ 
